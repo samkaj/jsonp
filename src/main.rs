2e6b1df031a48265736fe97cc53c787a5aebced9
samkaj <kajavasamuel@gmail.com>
2025-02-05 21:12:34 +0100

    [feat] read file path from args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use std::env;
+
 use jsonp::parse::Parser;
 use jsonp::tokenize::Tokenizer;
 
 fn main() -> Result<(), ()> {
-    let source = match std::fs::read_to_string("src/sample.json") {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <json-file>", args[0]);
+        return Err(());
+    }
+
+    let source = match std::fs::read_to_string(args[1].as_str()) {
         Ok(source) => source,
         Err(err) => {
             eprintln!("IO error: {}", err);
